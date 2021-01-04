5e6db46b2ddbaadcf98a8af4dcc80dab3e087046
Bálint Berente <balint.berente@gmail.com>
2021-01-04 23:12:32 +0100

    Callback url issue

--- webserver.js
+++ webserver.js
@@ -5,6 +5,9 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.get('/callback', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen(4000);
 app.addListener('error',(err)=>{
   console.log(err);
